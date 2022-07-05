6d20f2d452a9a18f84c45b5856b047ea4a16f6b7
Tomomi-K1 <tomomi.music@gmail.com>
2022-07-04 18:21:13 -0700

    first commit

---
+++ es6-10.js
@@ -0,0 +1,14 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+// ES2015 version
+const PI = 3.14;
+
+// What is the difference between var and let?
+// var can be redeclared, but let cannot be redeclared. let is block-scoped.
+// What is the difference between var and const?
+// const cannot be reassigned nor redeclared, but var can. const is block-scoped.
+// What is the difference between let and const?
+// const cannot be reasssigned, but let can be ressigned.
+// What is hoisting?
+// var will get lifted to the top of the scope that they are declared.
